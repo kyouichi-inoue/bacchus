153ea809395782807a371e8d55d3c0fa6b173ab3
kyouichi-inoue <corleonis81@gmail.com>
2020-01-20 19:12:02 +0900

    itemテーブル作成
    user_idのカラムはusersテーブルを作成した後に追加する。
    null:falseは後で追加する。

--- db/migrate/20200120083751_create_items.rb
+++ db/migrate/20200120083751_create_items.rb
@@ -1,8 +1,12 @@
 class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
-
-      t.timestamps
+      t.string :item
+      t.text :text
+      t.integer :liquor_id
+      t.integer :taste_id
+      t.integer :place_id
+      t.integer :alcohol_id
     end
   end
 end
