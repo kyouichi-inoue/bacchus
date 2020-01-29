3a4d7e5762b775d9f6bd32d1be7d4ac64cbd41bf
kyouichi-inoue <corleonis81@gmail.com>
2020-01-29 15:38:30 +0900

    liquorのid修正

--- app/models/liquor.rb
+++ app/models/liquor.rb
@@ -1,12 +1,12 @@
 class Liquor < ActiveHash::Base
   self.data = [
       {id: 1, name: '日本酒'},
-      {id: 3, name: '焼酎'},
-      {id: 2, name: 'ビール'},
-      {id: 3, name: 'ワイン'},
-      {id: 3, name: 'ウイスキー'},
-      {id: 3, name: 'ブランデー'},
-      {id: 3, name: 'カクテル各種'},
-      {id: 3, name: 'その他'},
+      {id: 2, name: '焼酎'},
+      {id: 3, name: 'ビール'},
+      {id: 4, name: 'ワイン'},
+      {id: 5, name: 'ウイスキー'},
+      {id: 6, name: 'ブランデー'},
+      {id: 7, name: 'カクテル各種'},
+      {id: 8, name: 'その他'},
   ]
 end
