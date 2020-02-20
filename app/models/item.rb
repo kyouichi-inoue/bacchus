c65f1f42f448523a3da7dce4205ec0fff33f35ab
kyouichi-inoue <corleonis81@gmail.com>
2020-02-20 19:02:32 +0900

    フォームのバリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,16 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
+  validates_associated :images
+  validates :item, presence: true
+  validates :text, presence: true
+  validates :liquor_id, presence: true
+  validates :place_id, presence: true
+  validates :taste_id, presence: true
+  validates :alcohol_id, presence: true
+  validates :images, presence: true
+  belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :liquor
