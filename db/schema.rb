a0521e869955abfd3da6e2948a2e711bbaf8d236
kyouichi-inoue <corleonis81@gmail.com>
2020-02-20 19:04:11 +0900

    画像を保存、schemaモデル変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200120083751) do
+ActiveRecord::Schema.define(version: 20200219083924) do
 
-  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string  "item"
-    t.text    "text",       limit: 65535
-    t.integer "liquor_id"
-    t.integer "taste_id"
-    t.integer "place_id"
-    t.integer "alcohol_id"
+  create_table "images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "image"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id", using: :btree
   end
 
+  create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "item"
+    t.text     "text",       limit: 65535
+    t.integer  "liquor_id"
+    t.integer  "taste_id"
+    t.integer  "place_id"
+    t.integer  "alcohol_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "user",                                null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "images", "items"
 end
